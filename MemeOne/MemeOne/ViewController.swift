d9e19307e232a864508f3488fe714afe5fbd500d
youssefibrahim1 <youssef.ibrahim@mail.utoronto.ca>
2020-04-06 00:48:30 -0400

    feedback-implementation

--- MemeOne/MemeOne/ViewController.swift
+++ MemeOne/MemeOne/ViewController.swift
@@ -7,6 +7,8 @@ import Foundation
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
+    
+    // PART 1 DECLARING OUTLETS AND VARIABLES
     @IBOutlet weak var topText: UITextField!
     
     @IBOutlet weak var bottomText: UITextField!
@@ -17,47 +19,87 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBOutlet weak var bottomStack: UIStackView!
     
-    var activeTextField = UITextField()
+    @IBOutlet weak var cameraButton: UIButton!
     
-    
+
     var imagePickerControl : UIImagePickerController!
     
+    
+    // PART 2 INITIALIZATION AND DEINITIALIZATION
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.topText.delegate = self
-        self.bottomText.delegate = self
+        topText.attributedPlaceholder = placeholderText(textDisplayed:  "TOP")
+        bottomText.attributedPlaceholder = placeholderText(textDisplayed:  "BOTTOM")
+
         
+        textConfiguration(textField: topText)
+        textConfiguration(textField: bottomText)
         
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            cameraButton.isEnabled = true
+        } else {
+            cameraButton.isEnabled = false
+        }
+
+        subscribeToKeyboardNotification()
         
-        let paragraph = NSMutableParagraphStyle()
-        paragraph.alignment = .center
+     
         
-        let memeTextAttributes: [NSAttributedString.Key: Any] = [
-            NSAttributedString.Key.strokeColor: UIColor.white,
-            NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1),
-            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-            NSAttributedString.Key.strokeWidth: -3,
-            NSAttributedString.Key.paragraphStyle: paragraph
-            
-        ]
-        
-        topText.defaultTextAttributes = memeTextAttributes
-        bottomText.defaultTextAttributes = memeTextAttributes
+     
         
     }
     
     
     deinit {
-        
+        unsubscribeToKeyboardNotification()
+    }
+    
+    
+    
+    // PART 3 ADDING AND REMOVING OBSERVERS
+
+    func subscribeToKeyboardNotification(){
+             NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+             NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+             NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    func unsubscribeToKeyboardNotification(){
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
     
+    // PART 4 TEXT CONFIGURATIONS
+    
+    func placeholderText(textDisplayed: String) -> NSAttributedString {
+        return NSAttributedString(string: textDisplayed, attributes: [NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.strokeColor: UIColor.black,                  NSAttributedString.Key.strokeWidth: -3,
+        ])
+    }
+    
+    func textConfiguration(textField: UITextField){
+        textField.delegate = self
+        
+        let paragraph = NSMutableParagraphStyle()
+             paragraph.alignment = .center
+             
+             let memeTextAttributes: [NSAttributedString.Key: Any] = [
+                 NSAttributedString.Key.strokeColor: UIColor.black,
+                 NSAttributedString.Key.foregroundColor: UIColor.white,
+                 NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+                 NSAttributedString.Key.strokeWidth: -3,
+                 NSAttributedString.Key.paragraphStyle: paragraph
+                 
+             ]
+             
+        textField.defaultTextAttributes = memeTextAttributes
+    }
+    
+    
+    // PART 5 BUTTON ACTIONS
+
     
     @IBAction func cancelMemeSelection(_ sender: Any) {
         topText.text = "TOP"
@@ -68,33 +110,49 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBAction func shareMeme(_ sender: Any) {
         let meme = generateMemedImage()
-        save(memeImageSave: meme)
         let vc = UIActivityViewController(activityItems: [meme], applicationActivities: [])
+        vc.completionWithItemsHandler = { (activityType: UIActivity.ActivityType?, completed:
+        Bool, arrayReturnedItems: [Any]?, error: Error?) in
+            self.save(memeImageSave: meme)
+            
+        }
         present(vc, animated: true)
         
     }
     
-    func save(memeImageSave: UIImage){
-        let saveMeme = Meme(topText: topText.text ?? "TOP", bottomText: bottomText.text ?? "BOTTOM", originalImage: memeHolder.image!, memeImage: memeImageSave)
+
+    
+    @IBAction func pickFromAlbum(_ sender: Any) {
+        let pickImage = UIImagePickerController()
+        pickImageFrom(chooseImage: pickImage, chooseType: "album")
+        present(pickImage, animated: true, completion: nil)
     }
     
     
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        self.activeTextField = textField
+    
+    @IBAction func takePicture(_ sender: Any) {
+            let pickImage = UIImagePickerController()
+            pickImageFrom(chooseImage: pickImage, chooseType: "Camera")
+
+            present(pickImage, animated: true, completion: nil)
     }
     
     
-    func hideKeyboard(){
-        topText.resignFirstResponder()
-        bottomText.resignFirstResponder()
-    }
+    // PART 6 KEYBOARD CONFIGURATIONS AND MEME GENERATION
+
     
+    func pickImageFrom(chooseImage: UIImagePickerController, chooseType: String){
+        chooseImage.delegate = self
+        if chooseType == "Camera" {
+            chooseImage.sourceType = .camera
+        } else if chooseType == "album" {
+            chooseImage.sourceType = .photoLibrary
+        }
+    }
     
     func generateMemedImage() -> UIImage {
 
-
-        let imageSize = CGSize(width: topStack.frame.size.width , height: self.view.frame.height ?? 511)
-
+        let imageSize = CGSize(width: topStack.frame.size.width , height: self.view.frame.height)
 
         let coordinates = CGRect(x: 0, y: bottomStack.frame.size.height, width: topStack.frame.size.width, height: memeHolder.frame.size.height)
 
@@ -122,8 +180,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             return
         }
         
-        if let activeText = self.activeTextField.placeholder {
-            if notification.name == UIResponder.keyboardWillShowNotification && activeText == "BOTTOM" {
+        if notification.name == UIResponder.keyboardWillShowNotification && bottomText.isFirstResponder {
                 view.frame.origin.y = -keyboardRect.height
                 
             } else {
@@ -131,9 +188,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
             }
 
-        }
+        
         
     }
+    
+    func save(memeImageSave: UIImage){
+         let saveMeme = Meme(topText: topText.text ?? "TOP", bottomText: bottomText.text ?? "BOTTOM", originalImage: memeHolder.image!, memeImage: memeImageSave)
+     }
+     
+     
+     
+     func hideKeyboard(){
+         topText.resignFirstResponder()
+         bottomText.resignFirstResponder()
+     }
     
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -146,31 +214,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let userInfo = notification.userInfo
         let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
         return keyboardSize.cgRectValue.height
-    }
-    
-
-    
-    @IBAction func pickFromAlbum(_ sender: Any) {
-        let pickImage = UIImagePickerController()
-        pickImage.delegate = self
-        pickImage.sourceType = .photoLibrary
-        present(pickImage, animated: true, completion: nil)
-    }
-    
-    
-    
-    @IBAction func takePicture(_ sender: Any) {
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            print("enters here")
-            let pickImage = UIImagePickerController()
-            pickImage.delegate = self
-            pickImage.sourceType = .camera
-            present(pickImage, animated: true, completion: nil)
-        } else {
-            let alert = UIAlertController(title: "Warning", message: "Your device doesn't seem to have a camera", preferredStyle: UIAlertController.Style.alert)
-            alert.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
-            present(alert, animated: true, completion: nil)
-        }
     }
     
     
